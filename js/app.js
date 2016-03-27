b8f4d28de637cf7a408c9fedce6eafe708e89f2c
Johannes Siipola <johannes@siipo.la>
2016-03-27 19:07:01 +0300

    Handle login

--- js/app.js
+++ js/app.js
@@ -22,7 +22,10 @@ class LoginBox extends React.Component {
             },
             body: JSON.stringify(credentials)
             }).then(response => response.json())
-            .then(data => {alert(data.token)})
+            .then(data => {
+                localStorage.setItem('token', data.token);
+                this.props.onLogin();
+            })
     }
 
     onEmailInput(event){
@@ -35,20 +38,22 @@ class LoginBox extends React.Component {
 
     render() {
         return (
-            <div className="login-box">
-                <div className="login-box-left"></div>
-                <div className="login-box-right">
-                    <h1 className="login-header">User Login</h1>
-                    <div className="form-group">
-                        <label htmlFor="email">Email Address</label>
-                        <input className="form-control" type="text" id="email" onInput={this.onEmailInput.bind(this)}/>
+            <div className="login-container">
+                <div className="login-box">
+                    <div className="login-box-left"></div>
+                    <div className="login-box-right">
+                        <h1 className="login-header">User Login</h1>
+                        <div className="form-group">
+                            <label htmlFor="email">Email Address</label>
+                            <input className="form-control" type="text" id="email" onInput={this.onEmailInput.bind(this)}/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input className="form-control" type="password" id="password" onInput={this.onPasswordInput.bind(this)}/>
+                        </div>
+                        <button className="btn btn-link pull-left">Lost password?</button>
+                        <button className="btn btn-primary pull-right" onClick={this.onLoginClick.bind(this)}>Login</button>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input className="form-control" type="password" id="password" onInput={this.onPasswordInput.bind(this)}/>
-                    </div>
-                    <button className="btn btn-link pull-left">Lost password?</button>
-                    <button className="btn btn-primary pull-right" onClick={this.onLoginClick.bind(this)}>Login</button>
                 </div>
             </div>
         );
@@ -56,12 +61,23 @@ class LoginBox extends React.Component {
 }
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {loggedIn: false};
+    }
+    handleOnLogin() {
+        this.setState({loggedIn: true});
+    }
     render() {
-        return (
-            <div className="login-container">
-                <LoginBox></LoginBox>
-            </div>
-        );
+        if (this.state.loggedIn) {
+            return (
+                <div><h1>You are logged in</h1></div>
+            )
+        } else {
+            return (
+                <LoginBox onLogin={this.handleOnLogin.bind(this)}></LoginBox>
+            )
+        }
     }
 }
 
