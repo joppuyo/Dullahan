7f103166270144ef6409fdbd881a080b6690afc9
Johannes Siipola <johannes@siipo.la>
2016-03-31 00:03:47 +0300

    Add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
     gulp.src('scss/style.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('css/'));
 });
 
