fccb2c7e436b97fa1bf8bef4deb4f8208794b7ca
Johannes Siipola <johannes@siipo.la>
2016-07-29 04:08:52 +0300

    List content in client

--- js/components/Content.jsx
+++ js/components/Content.jsx
@@ -5,39 +5,67 @@ import SectionHeaderLeft from './SectionHeaderLeft.jsx';
 import SectionHeaderRight from './SectionHeaderRight.jsx';
 import FetchService from '../services/FetchService';
 import _ from 'underscore';
+import ListItem from './ListItem.jsx';
 
 export default class Content extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             contentTypes: [],
+            content: [],
         };
     }
 
     componentDidMount() {
         FetchService.get('api/content').then((data) => {
             this.setState(_.extend(this.state, { contentTypes: data }));
+
+            // Get the initial data
+            if (data && data[0]) {
+                this.getContent(data[0].slug);
+            }
+        });
+    }
+
+    onContentTypeSelect(event) {
+        let contentTypeSlug = event.target.value;
+        this.getContent(contentTypeSlug);
+    }
+
+    getContent(contentTypeSlug) {
+        FetchService.get(`api/content/${contentTypeSlug}`).then((data) => {
+            this.setState(_.extend(this.state, { content: data }));
         });
     }
 
     render() {
         return (
             <DocumentTitle title="Content - Dullahan">
-                <SectionHeader title="Content">
-                    <SectionHeaderLeft>
-                        <label>
-                            <div className="custom-select content-type-selector">
-                                <select>
-                                    {this.state.contentTypes.map((contentType) => <option value={contentType.slug}>{contentType.name}</option>)});}
-                                </select>
-                            </div>
-                        </label>
-                    </SectionHeaderLeft>
-                    <SectionHeaderRight>
-                        <a href="#" className="btn btn-primary">Add new content</a>
-                    </SectionHeaderRight>
-                </SectionHeader>
+                <div className="section-wrapper">
+                    <SectionHeader title="Content">
+                        <SectionHeaderLeft>
+                            <label>
+                                <div className="custom-select content-type-selector">
+                                    <select onChange={this.onContentTypeSelect.bind(this)}>
+                                        {this.state.contentTypes.map((contentType) => <option value={contentType.slug} key={contentType.slug}>{contentType.name}</option>)});}
+                                    </select>
+                                </div>
+                            </label>
+                        </SectionHeaderLeft>
+                        <SectionHeaderRight>
+                            <a href="#" className="btn btn-primary">Add new content</a>
+                        </SectionHeaderRight>
+                    </SectionHeader>
+                    {this.state.content.map(item =>
+                        <ListItem
+                            title={item._title}
+                            subtitle="Subtitle"
+                            key={item._id}
+                            image={item._image}
+                        />
+                    )}
+                </div>
             </DocumentTitle>
-        )
+        );
     }
 }
--- js/components/ListItem.jsx
+++ js/components/ListItem.jsx
@@ -3,9 +3,13 @@ import { Router, Route, Link, hashHistory } from 'react-router';
 
 export default class ListItem extends React.Component {
     render() {
+        let style = null;
+        if (this.props.image) {
+            style = { backgroundImage: `url(${this.props.image})` };
+        }
         return (
             <div className="list-item clearfix">
-                <div className="list-item-icon"/>
+                <div className="list-item-icon" style={style} />
                 <div className="list-item-right">
                     <div className="list-item-title">{this.props.title}</div>
                     <div className="list-item-subtitle">{this.props.subtitle}</div>
