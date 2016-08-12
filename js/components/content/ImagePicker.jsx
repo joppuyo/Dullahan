2f59e34cfd31e3f7578e3ff60bc3711e685b61ef
Johannes Siipola <johannes@siipo.la>
2016-08-12 21:33:08 +0300

    Fix media picker overflow

--- js/components/content/ImagePicker.jsx
+++ js/components/content/ImagePicker.jsx
@@ -30,15 +30,17 @@ export default class ImagePicker extends React.Component {
         }
         return (
             <div className={classes}>
+                <div className="section-wrapper">
                 <div className="section-header">
                     <h1>Select image</h1>
                     <div className="section-header-right">
                     <button className="btn btn-default" onClick={this.props.closeImagePicker.bind(this)}>Cancel</button>
                     </div>
                 </div>
-                <div className="content-create-media-items-container">
+                <div className="media-items-container">
                     {this.state.images.map(image => <MediaItemSelect onSelectImage={this.onSelectImage.bind(this)} {...image} key={image.full_name}/>)}
                 </div>
+            </div>
             </div>
         );
     }
