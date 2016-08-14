1504a219713afea52fe0454ce4f701f670cce0c5
Johannes Siipola <johannes@siipo.la>
2016-08-14 13:39:42 +0300

    Improve routing

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, hashHistory } from 'react-router';
+import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router';
 
 import App from './components/App.jsx';
 import LoginBox from './components/LoginBox.jsx';
@@ -18,17 +18,17 @@ import Apps from './components/apps/Apps.jsx';
 ReactDOM.render(
     <DocumentTitle title="Dullahan">
         <Router history={hashHistory}>
-            <Route path="/" component={App} />
             <Route path="/login" component={LoginBox} />
-            <Route path="/app" component={Dashboard}>
-                <Route path="/content" component={Content} />
-                <Route path="/content/create/:contentTypeSlug" component={ContentCreate} />
-                <Route path="/content/:contentId" component={ContentView} />
-                <Route path="/content/:contentId/edit" component={ContentUpdate} />
-                <Route path="/media" component={Media} />
-                <Route path="/users" component={Users} />
-                <Route path="/users/add" component={AddUser} />
-                <Route path="/apps" component={Apps} />
+            <Route path="/" component={Dashboard}>
+                <IndexRoute component={App} />
+                <Route path="content" component={Content} />
+                <Route path="content/create/:contentTypeSlug" component={ContentCreate} />
+                <Route path="content/:contentId" component={ContentView} />
+                <Route path="content/:contentId/edit" component={ContentUpdate} />
+                <Route path="media" component={Media} />
+                <Route path="users" component={Users} />
+                <Route path="users/add" component={AddUser} />
+                <Route path="apps" component={Apps} />
             </Route>
         </Router>
     </DocumentTitle>, document.getElementById('app')
